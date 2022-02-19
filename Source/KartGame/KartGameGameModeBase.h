1877a738cc3d6530664b2e17d7921048ec0bbc5e
João Pedro Casella de Paula <joao.casella@fanatee.com>
2022-02-19 12:21:52 -0300

    Init C++ project

---
+++ Source/KartGame/KartGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KartGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARTGAME_API AKartGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
